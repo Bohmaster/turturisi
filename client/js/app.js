e5af3cd9976947e2c75b2024b9ba33fec9b71fb3
Bohmaster <voidhmaster@gmail.com>
2015-11-26 14:09:43 -0300

    commit

---
+++ client/js/app.js
@@ -0,0 +1,214 @@
+'use strict';
+var myApp = angular.module('app', [
+	'ui.bootstrap',
+  'ngResource',
+	'lbServices'
+]);
+
+myApp.controller('AdminController', function($scope, $http, $window, Novedad, Archivo) {
+  function getNovedades() {
+    Novedad.find(function(data) {
+      angular.forEach(data, function(value, key) {
+        console.log(value, key);
+        value.fecha = moment(value.fecha).format('YYYY-MM-DD');
+      });
+      console.log(data);
+      $scope.novedades = data;
+    });
+  }
+
+  function getFiles() {
+    Archivo.find({
+      filter: {
+        where: {
+          tipo: 'eaton'
+        }
+      }
+    }, function(data) {
+      $scope.eaton = data[0];
+      console.log(2222, data[0]);
+    }, function(err) {
+      console.log(err);
+    });
+
+    Archivo.find({
+      filter: {
+        where: {
+          tipo: 'zf'
+        }
+      }
+    }, function(data) {
+      $scope.zf = data[0];
+      console.log(2222, data[0]);
+    }, function(err) {
+      console.log(err);
+    });
+
+    Archivo.find({
+      filter: {
+        where: {
+          tipo: 'list'
+        }
+      }
+    }, function(data) {
+      $scope.lista = data[0];
+      console.log(2222, data[0]);
+    }, function(err) {
+      console.log(err);
+    });
+  }
+
+  getFiles();
+
+  getNovedades();
+
+  $scope.novedad = {
+    fecha: moment().format('YYYY-MM-DD'),
+    descripcion: ''
+  };
+
+  $scope.agregarNovedad = function() {
+    Novedad.create({
+      fecha: $scope.novedad.fecha,
+      descripcion: $scope.novedad.descripcion
+    }, function(data) {
+      getNovedades();
+      console.log(data);
+
+    }, function(err) {
+      console.log(err);
+    });
+  };
+
+  $scope.toDelete = [];
+
+  $scope.addToRemove = function(id) {
+    function include(arr,obj) {
+      return (arr.indexOf(obj) != -1);
+    }
+    if (include($scope.toDelete, id)) {
+      var index = $scope.toDelete.indexOf(id);
+      $scope.toDelete.splice(index, 1);
+    } else {
+      $scope.toDelete.push(id);
+    }
+  };
+
+  $scope.removeSelected = function() {
+    angular.forEach($scope.toDelete, function(value, key) {
+      console.log(key, value);
+      $http.delete('http://localhost:3000/api/Novedads/' + value)
+        .success(function(data) {
+          getNovedades();
+        })
+        .error(function(err) {
+          console.log(err);
+        });
+    });
+  };
+
+  $scope.result = "";
+
+  $scope.download = function(name) {
+    console.log($scope.eaton);
+    if (name === 'eaton') {
+      $window.location.assign('http://localhost:3000/api/containers/files/download/' + $scope.eaton.imagen);
+    } else if (name === "zf") {
+      $window.location.assign('http://localhost:3000/api/containers/files/download/' + $scope.zf.imagen);
+    }
+  };
+
+  $scope.auth = function() {
+      console.log('auth');
+      var pw = prompt("Por favor ingrese la contraseña");
+      if (pw == "asd") {
+        window.location.assign('http://localhost:3000/api/containers/files/download/' + $scope.lista.imagen);
+      } else if (pw == "admin") {
+        window.location.assign('http://localhost:3000/admin.html');
+      }
+  };
+
+  $scope.upload = function() {
+
+    console.log($scope.type);
+
+      var fd = new FormData();
+
+      angular.forEach($scope.file, function(file) {
+        fd.append('file', file);
+      });
+
+     console.log($scope.file);
+
+     console.log(fd);
+
+     $http.post('/api/containers/files/upload',
+        fd, {
+          transformRequest: angular.identity,
+          headers: {'Content-Type': undefined}
+        }
+      )
+        .success(function(d){
+          console.log(d);
+          $scope.result = "El archivo " + $scope.file[0].name + " se ha subido correctamente";
+          Archivo.create({
+            tipo: $scope.type,
+            imagen: $scope.file[0].name
+          }, function(success) {
+            console.log(success);
+          });
+        })
+        .error(function(e) {
+          console.log(e);
+        });
+    };
+
+});
+
+myApp.directive('eaton', function($parse) {
+    return {
+      restrict: 'A',
+      link: function(scope, elm, attrs) {
+        elm.bind('change', function() {
+          $parse(attrs.eaton)
+            .assign(scope, elm[0].files);
+          scope.type = "eaton";
+          scope.$apply();
+
+          console.log(scope.file[0].name);
+        });
+      }
+    }
+  });
+
+myApp.directive('zf', function($parse) {
+    return {
+      restrict: 'A',
+      link: function(scope, elm, attrs) {
+        elm.bind('change', function() {
+          $parse(attrs.zf)
+            .assign(scope, elm[0].files);
+          scope.type = "zf";
+          scope.$apply();
+
+          console.log(scope.file[0].name);
+        });
+      }
+    }
+  });
+
+myApp.directive('priceList', function($parse) {
+    return {
+      restrict: 'A',
+      link: function(scope, elm, attrs) {
+        elm.bind('change', function() {
+          $parse(attrs.priceList)
+            .assign(scope, elm[0].files);
+          scope.type = "list";
+          scope.$apply();
+
+          console.log(scope.file[0].name);
+        });
+      }
+    }
+  });
---
+++ server/server.js
@@ -0,0 +1,35 @@
+var loopback = require('loopback');
+var boot = require('loopback-boot');
+var path = require('path');
+
+var app = module.exports = loopback();
+
+// storage
+var ds = loopback.createDataSource({
+  connector: require('loopback-component-storage'),
+  provider: 'filesystem',
+  root: path.join(__dirname, 'storage')
+});
+var Container = ds.createModel('container');
+app.model(Container);
+
+// static
+app.use(loopback.static(path.resolve(__dirname)));
+
+app.start = function() {
+  // start the web server
+  return app.listen(function() {
+    app.emit('started');
+    console.log('Web server listening at: %s', app.get('url'));
+  });
+};
+
+// Bootstrap the application, configure models, datasources and middleware.
+// Sub-apps like REST API are mounted via boot scripts.
+boot(app, __dirname, function(err) {
+  if (err) throw err;
+
+  // start the server if `$ node server.js`
+  if (require.main === module)
+    app.start();
+});
